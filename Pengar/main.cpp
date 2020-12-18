0763cc0e6b93cd47fffada18f596dc650b6e2d92
Jakob <jakob@salomonssons.se>
2020-12-18 13:12:12 +0100

    Initial Commit

---
+++ Pengar/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pengar
+//
+//  Created by Jakob Salomonsson on 2020-12-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
